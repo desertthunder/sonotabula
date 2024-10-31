0bdf58de1b02963de9a44c43fd586578a214e3f1
Owais Jamil <desertthunder.dev@gmail.com>
2024-10-30 23:25:51 -0500

    build: ruff docstring rule change

--- core/tests.py
+++ core/tests.py
@@ -15,3 +15,36 @@ class CoreViewsTestCase(TestCase):
     def test_user_saved_items_view(self) -> None:
         """Test user saved items view."""
         pass
+
+
+class AppUserManagerTestCase(TestCase):
+    """AppUserManager test case."""
+
+    @unittest.skip("TODO")
+    def test_from_spotify(self) -> None:
+        """Test from_spotify method."""
+        pass
+
+
+class AppUserTestCase(TestCase):
+    """AppUser model tests."""
+
+    @unittest.skip("TODO")
+    def test_str(self) -> None:
+        """Test __str__ method."""
+        pass
+
+    @unittest.skip("TODO")
+    def test_should_update(self) -> None:
+        """Test should_update method."""
+        pass
+
+    @unittest.skip("TODO")
+    def test_update_token_set(self) -> None:
+        """Test update_token_set method."""
+        pass
+
+    @unittest.skip("Not implemented")
+    def test_should_refresh_token(self) -> None:
+        """Test should_refresh_token method."""
+        pass
--- live/apps.py
+++ live/apps.py
@@ -1,10 +1,6 @@
-"""Live app configuration."""
-
 from django.apps import AppConfig
 
 
 class LiveConfig(AppConfig):
-    """Live app configuration."""
-
     default_auto_field = "django.db.models.BigAutoField"
     name = "live"
